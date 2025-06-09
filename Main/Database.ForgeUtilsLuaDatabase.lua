f3ebc7e3dd7d034bcadd681f6731977ae4bc2be4
Thomas <64567080+Distantz@users.noreply.github.com>
2025-06-09 14:29:51 +1200

    feat: add db manager

--- Main/Database.ForgeUtilsLuaDatabase.lua
+++ Main/Database.ForgeUtilsLuaDatabase.lua
@@ -6,5 +6,6 @@ local ForgeUtilsLuaDatabase = module(...)
 ForgeUtilsLuaDatabase.AddContentToCall = function(_tContentToCall)
     -- We tell the Database Manager to load our custom Database Manager by its Lua name
     -- You can add as many as you want, ideally separating each manager for each content type you add
-    -- table.insert(_tContentToCall, require("Database.ExampleContentDatabaseManager"))
+    table.insert(_tContentToCall, require("Database.ForgeUtilsDatabaseManager"))
 end
+
