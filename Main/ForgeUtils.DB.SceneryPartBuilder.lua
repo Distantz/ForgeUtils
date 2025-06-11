5565204763798690a3bf6736254d30e443642dea
Thomas <64567080+Distantz@users.noreply.github.com>
2025-06-11 12:01:21 +1200

    fix: try workaround luals issue

--- Main/ForgeUtils.DB.SceneryPartBuilder.lua
+++ Main/ForgeUtils.DB.SceneryPartBuilder.lua
@@ -2,7 +2,7 @@ local global = _G
 local api = global.api
 local setmetatable = global.setmetatable
 
---- @class ForgeUtils.DB.SceneryPartBuilder : table
+--- @class ForgeUtils.DB.SceneryPartBuilder
 --- @field contentPack string
 --- @field contentPackID integer
 --- @field dataPrefab string
