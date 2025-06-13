bbc48ce28e0939395495381c790e19bf7a8dac55
Thomas <64567080+Distantz@users.noreply.github.com>
2025-06-13 19:59:35 +1200

    feat: try include empty namespace files to correctly generate docs

---
+++ Main/forgeutils.lua
@@ -0,0 +1,9 @@
+---@meta
+error("Do not try to execute this file. It's just a type definition file.")
+--- Namespace
+--------------------------------------------------------------------------------
+---## forgeutils
+
+---Namespace for renoise view widgets.
+---@class forgeutils
+local forgeutils = {}
---
+++ Main/forgeutils/builders.lua
@@ -0,0 +1,9 @@
+---@meta
+error("Do not try to execute this file. It's just a type definition file.")
+--- Namespace
+--------------------------------------------------------------------------------
+---## forgeutils.builders
+
+---Namespace for renoise view widgets.
+---@class forgeutils.builders
+local forgeutilsbuilders = {}
