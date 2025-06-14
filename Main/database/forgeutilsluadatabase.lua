de473bc8d92d4d59a1bc1102e975512dc961353c
Thomas <64567080+Distantz@users.noreply.github.com>
2025-06-14 18:31:55 +1200

    feat: add moddb manager, which mods can register to in order to ensure installed forgeutils is up to date enough

--- Main/database/forgeutilsluadatabase.lua
+++ Main/database/forgeutilsluadatabase.lua
@@ -3,19 +3,78 @@
 local global = _G
 local table = global.table
 local require = require
+local pairs = global.pairs
 
 -- Setup logger
-local loggerSetup = require("ForgeUtils.Logger")
+local loggerSetup = require("forgeutils.logger")
 local logger = loggerSetup.Get("ForgeUtilsLuaDatabase")
 
 logger:Info("Starting ForgeUtils...")
 
 local _ForgeUtilsLuaDatabase = {}
+_ForgeUtilsLuaDatabase.hasShownPopup = false
 
-_ForgeUtilsLuaDatabase.AddContentToCall = function(_tContentToCall)
+function _ForgeUtilsLuaDatabase.AddContentToCall(_tContentToCall)
     -- We tell the Database Manager to load our custom Database Manager by its Lua name
     -- You can add as many as you want, ideally separating each manager for each content type you add
-    table.insert(_tContentToCall, require("ForgeUtils.Internal.Database.Scenery"))
+    table.insert(_tContentToCall, require("database.forgeutilsluadatabase"))
+    table.insert(_tContentToCall, require("forgeutils.internal.database.scenery"))
+end
+
+function _ForgeUtilsLuaDatabase._Hook_StartScreenPopupHelper_RunCheckLocalModification(tModule)
+    tModule.RunCheckLocalModification_Base = tModule._RunCheckLocalModification
+    tModule._RunCheckLocalModification = function(self)
+        if _ForgeUtilsLuaDatabase.hasShownPopup then
+            tModule.RunCheckLocalModification_Base(self)
+            return
+        end
+
+        -- Check if we actually have any out of date mods
+        local moddb = require("forgeutils.moddb")
+        local outOfDate = moddb.GetModsOutOfDate()
+        local foundOutOfDate = false
+
+        local lines = {
+            "Some installed mods require a newer version of ForgeUtils. Consider updating if things don't work.\n",
+            "ForgeUtils: " .. tostring(moddb.CURRENT_VERSION)
+        }
+
+        for mod, ver in pairs(outOfDate) do
+            local line = tostring(mod) .. ": " .. tostring(ver)
+            logger:Warn(line)
+            table.insert(lines, line)
+            foundOutOfDate = true
+        end
+
+        local stringBuilder = "[STRING_LITERAL:Value=|" .. table.concat(lines, "\n") .. "|]"
+
+        _ForgeUtilsLuaDatabase.hasShownPopup = true
+
+        if not foundOutOfDate then
+            return
+        end
+
+        local popup = require("Helpers.PopUpDialogUtils")
+        popup.RunOKDialog(
+            "[STRING_LITERAL:Value=|ForgeUtils version|]",
+            stringBuilder
+        )
+
+        tModule.RunCheckLocalModification_Base(self) -- run base
+    end
+end
+
+_ForgeUtilsLuaDatabase.tDefaultHooks = {
+    ["StartScreen.Shared.StartScreenPopupHelper"] = _ForgeUtilsLuaDatabase
+        ._Hook_StartScreenPopupHelper_RunCheckLocalModification
+}
+
+function _ForgeUtilsLuaDatabase.AddLuaHooks(_fnAdd)
+    logger:Info("Adding hooks")
+    for sModuleName, tFunc in pairs(_ForgeUtilsLuaDatabase.tDefaultHooks) do
+        logger:Info("Adding hook for module [" .. sModuleName .. "]")
+        _fnAdd(sModuleName, tFunc)
+    end
 end
 
 return _ForgeUtilsLuaDatabase
---
+++ Main/forgeutils/moddb.lua
@@ -0,0 +1,50 @@
+local global = _G
+local table = global.table
+local require = require
+local pairs = global.pairs
+
+-- Setup logger
+local loggerSetup = require("forgeutils.logger")
+local logger = loggerSetup.Get("ForgeUtilsModDB", "DEBUG")
+
+---@class forgeutils.ModDB
+---@field registeredMods {[string]: number } Registered mods with the DB.
+---@field CURRENT_VERSION number The current version for ForgeUtils.
+local ModDB = {}
+ModDB.registeredMods = {}
+
+-- Set current version whenever a new release occurs.
+ModDB.CURRENT_VERSION = 1.0
+
+---Registers a mod with the ModDB.
+---If a mod is using a version that is higher than the installed ForgeUtils mod,
+---it will give an notification for the user to update on the main menu.
+---@param modName string The name to display to the user.
+---@param usingVersion number|nil The version that this mod was developed for. If nil, will be installed version.
+function ModDB.RegisterMod(modName, usingVersion)
+    if usingVersion == nil then
+        usingVersion = ModDB.CURRENT_VERSION
+    end
+    logger:Info("Registered new mod with ForgeUtils: " .. tostring(modName) .. "[" .. usingVersion .. "]")
+    ModDB.registeredMods[modName] = usingVersion
+    logger:Info("Registered Mods: ")
+    for mod, ver in pairs(ModDB.registeredMods) do
+        logger:Info(mod)
+    end
+end
+
+---Returns any registered mods that are out of date.
+---@return {[string]: number } mods Out of date mods, key being name and value being version.
+function ModDB.GetModsOutOfDate()
+    local results = {}
+    for mod, ver in pairs(ModDB.registeredMods) do
+        logger:Info("Found mod: " .. mod)
+        if ver > ModDB.CURRENT_VERSION then
+            logger:Info("OUT OF DATE: " .. mod)
+            results[mod] = ver
+        end
+    end
+    return results
+end
+
+return ModDB
