ab0ae0afcc161d575de484bba43afdf8104a606e
Thomas <64567080+Distantz@users.noreply.github.com>
2026-03-08 15:47:49 +1300

    31 fix deployment pipeline and build scripts (#32)
    * feat: upgrade build system, update pipelines, prepare for better pipeline stuff
    
    * fix: cobra tools issues
    
    * feat: refactor version tracking, begin work on release
    
    * fix: add version validation, fix cobra tools buggin
    
    * feat: add release and move forge to lower version for testing

--- Main/forgeutils.lua
+++ Main/forgeutils.lua
@@ -1,9 +1,22 @@
 ---This is a simple namespace for forgeutils.
 ---Contains the version of the mod.
+
+---@class forgeutils.Version Semantic versioning type.
+---@field major integer? The major (breaking change) number.
+---@field minor integer? The minor (new non-breaking addition) number.
+---@field patch integer? The patch (non-breaking bug fix) number.
+
 ---@class forgeutils
----@field version number The version of ForgeUtils
+---@field version forgeutils.Version The version of ForgeUtils.
 local ForgeUtils = {}
 
-ForgeUtils.version = 1.52
+-- Note: Do not change the indentation.
+-- This is modified and read by pipelines.
+-- Modify data in place.
+ForgeUtils.version = {
+    major = 1,
+    minor = 6,
+    patch = 0,
+}
 
 return ForgeUtils
--- Main/forgeutils/moddb.lua
+++ Main/forgeutils/moddb.lua
@@ -6,10 +6,10 @@ local ForgeUtils = require("forgeutils")
 
 -- Setup logger
 local loggerSetup = require("forgeutils.logger")
-local logger = loggerSetup.Get("ForgeUtilsModDB")
+local logger = loggerSetup.Get("ForgeUtilsModDB", "INFO")
 
 ---@class forgeutils.ModDB
----@field registeredMods {[string]: number } Registered mods with the DB.
+---@field registeredMods {[string]: forgeutils.Version } Registered mods with the DB.
 local ModDB = {}
 ModDB.registeredMods = {}
 
@@ -17,27 +17,89 @@ ModDB.registeredMods = {}
 ---If a mod is using a version that is higher than the installed ForgeUtils mod,
 ---it will give an notification for the user to update on the main menu.
 ---@param modName string The name to display to the user.
----@param usingVersion number|nil The version that this mod was developed for. If nil, will be installed version.
-function ModDB.RegisterMod(modName, usingVersion)
-    if usingVersion == nil then
-        usingVersion = ForgeUtils.version
+---@param minimumMajorVersion integer? The minimum major version required. If nil, not checked.
+---@param minimumMinorVersion integer? The minimum minor version required. If nil, not checked.
+---@param minimumPatchVersion integer? The minimum patch version required. If nil, not checked.
+function ModDB.RegisterMod(
+    modName,
+    minimumMajorVersion,
+    minimumMinorVersion,
+    minimumPatchVersion
+)
+    -- backwards compat. make required major version the floor of a float version (previously used)
+    if minimumMajorVersion ~= nil and global.type(minimumMajorVersion) == "number" then
+        logger:Warn("Float version numbers have been deprecated. Please use the latest function to register mods.")
+        minimumMajorVersion = global.math.floor(minimumMajorVersion)
     end
-    logger:Info("Registered new mod with ForgeUtils: " .. tostring(modName) .. "[" .. usingVersion .. "]")
-    ModDB.registeredMods[modName] = usingVersion
-    logger:Info("Registered Mods: ")
-    for mod, ver in pairs(ModDB.registeredMods) do
-        logger:Info(mod)
+
+    logger:Info("Registered new mod with ForgeUtils: " .. global.tostring(modName))
+    ModDB.registeredMods[modName] = {
+        major = minimumMajorVersion,
+        minor = minimumMinorVersion,
+        patch = minimumPatchVersion
+    }
+
+    logger:Info("Checking version compatibility...")
+end
+
+local function performSemanticCheck(modValue, forgeValue, shouldBeEqual)
+    if modValue == nil or forgeValue == nil then
+        return true
     end
+
+    -- used for major
+    if shouldBeEqual then
+        return modValue == forgeValue
+    end
+
+    return modValue >= forgeValue
+end
+
+local function logSemanticErrorStr(modName, semanticType, modValue, forgeValue)
+    return (
+        "Mod [" ..
+        modName ..
+        "] version mismatch. " ..
+        semanticType ..
+        " version of mod is " ..
+        global.tostring(modValue) ..
+        ", current ForgeUtils is " ..
+        global.tostring(forgeValue) ..
+        ". You should update your ForgeUtils!"
+    )
+end
+
+--- Checks the mod version against the current install. If
+---@param modName string The name to display to the user.
+---@return boolean inDate Whether the mod was in date.
+function ModDB.CheckVersionForMod(modName)
+    local version = ModDB.registeredMods[modName]
+
+    if not performSemanticCheck(version.major, ForgeUtils.version.major, true) then
+        logger:Error(logSemanticErrorStr(modName, "major", version.major, ForgeUtils.version.major))
+        return false
+    end
+
+    if not performSemanticCheck(version.minor, ForgeUtils.version.minor, false) then
+        logger:Error(logSemanticErrorStr(modName, "minor", version.minor, ForgeUtils.version.minor))
+        return false
+    end
+
+    if not performSemanticCheck(version.patch, ForgeUtils.version.patch, false) then
+        logger:Error(logSemanticErrorStr(modName, "patch", version.patch, ForgeUtils.version.patch))
+        return false
+    end
+
+    return true
 end
 
 ---Returns any registered mods that are out of date.
----@return {[string]: number } mods Out of date mods, key being name and value being version.
+---@return {[string]: forgeutils.Version } mods Out of date mods, key being name and value being version.
 function ModDB.GetModsOutOfDate()
     local results = {}
     for mod, ver in pairs(ModDB.registeredMods) do
-        logger:Info("Found mod: " .. mod)
-        if ver > ForgeUtils.version then
-            logger:Info("OUT OF DATE: " .. mod)
+        if not ModDB.CheckVersionForMod(mod) then
+            logger:Error("OUT OF DATE: " .. mod)
             results[mod] = ver
         end
     end
