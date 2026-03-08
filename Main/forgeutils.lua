92d17cdf3c4fc5dae914f00e450cd3fd4162510f
Coppertine <37494321+Coppertine@users.noreply.github.com>
2026-03-08 20:09:19 +1000

    feat: add version to string through seperate class

--- Main/forgeutils.lua
+++ Main/forgeutils.lua
@@ -1,5 +1,6 @@
 ---This is a simple namespace for forgeutils.
 ---Contains the version of the mod.
+local version = require("forgeutils.modversion")
 
 ---@class forgeutils.Version Semantic versioning type.
 ---@field major integer? The major (breaking change) number.
@@ -13,10 +14,10 @@ local ForgeUtils = {}
 -- Note: Do not change the indentation.
 -- This is modified and read by pipelines.
 -- Modify data in place.
-ForgeUtils.version = {
+ForgeUtils.version = version.new({
     major = 2,
     minor = 0,
-    patch = 0,
-}
+    patch = 0
+})
 
 return ForgeUtils
