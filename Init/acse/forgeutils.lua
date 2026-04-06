0d661267cb31020367d5d07c612b1a45df824590
Thomas <64567080+Distantz@users.noreply.github.com>
2026-04-06 18:30:44 +1200

    feat: rework hook functionality to be lazy loaded

---
+++ Init/acse/forgeutils.lua
@@ -0,0 +1,81 @@
+local global           = _G
+---@type Api
+---@diagnostic disable-next-line: undefined-field
+local api              = global.api
+local package          = global.package
+local tostring         = global.tostring
+local string           = global.string
+
+local ForgeUtils       = {}
+
+-- Code taken from ACSEDebug. Thank you Inaki!
+local API_ReloadModule = function(sModuleName)
+    api.debug.Trace("Reloading module: " .. tostring(sModuleName))
+    sModuleName = string.lower(sModuleName)
+    local tMod = package.loaded[sModuleName]
+    if global.type(tMod) ~= "table" then
+        local sErr = "Module not loaded, not reloading."
+        api.debug.Trace(sErr)
+        return false, sErr
+    end
+
+    local fnModLoadError = function(sStage, sErrorMessage)
+        local sErr = "Module reload failed, error while attempting to " .. sStage .. " module:\n\n" .. sErrorMessage
+        api.debug.Trace(sErr)
+        return false, sErr
+    end
+
+    ---@diagnostic disable-next-line: undefined-field
+    local fnMod, sErrorMessage = global.loadresource(sModuleName)
+    if not fnMod then
+        return fnModLoadError("load", sErrorMessage)
+    end
+
+    ---@diagnostic disable-next-line: undefined-field
+    local module = global.tryrequire(sModuleName)
+    if module ~= nil then
+        module.s_tInterfaces = nil
+    end
+    local bOk = nil
+    bOk = global.pcall(fnMod, sModuleName)
+    if not bOk then
+        return fnModLoadError("execute", sErrorMessage)
+    end
+    return true
+end
+
+---@alias ForgeUtilsRequireCallback fun(moduleName : string, requireReturned : any): nil
+
+ForgeUtils.OnInit      = function()
+    ---@diagnostic disable-next-line: inject-field
+    api.forgeutils = {
+
+        ---@type ForgeUtilsRequireCallback
+        OnRequiredCallback = function(moduleName, moduleValue)
+        end,
+
+        OldRequire = global.require
+    }
+
+    --- Patch and load our required callback.
+    ---@diagnostic disable-next-line: inject-field
+    global.require = function(modName)
+        local moduleName = string.lower(modName)
+        local newLoad = not package.loaded[moduleName]
+
+        local requiredValue = api.forgeutils.OldRequire(modName)
+
+        if newLoad then
+            api.forgeutils.OnRequiredCallback(moduleName, requiredValue)
+        end
+
+        return requiredValue
+    end
+
+    -- Reload Environment module to refresh all the modified APIS of that module (specially require())
+    API_ReloadModule('Environment.Environment')
+    API_ReloadModule('Game.BaseGame')
+    API_ReloadModule('Game.GameScript')
+end
+
+return ForgeUtils
